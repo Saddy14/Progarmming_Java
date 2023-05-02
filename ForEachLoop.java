653d41740130b8a00881bfe554a81469bf83bc57
Saddy14 <1221301874@student.mmu.edu.my>
2023-05-02 08:57:57 +0800

    ForEachLoop

--- ForEachLoop.java
+++ ForEachLoop.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+
 public class ForEachLoop {
     public static void main(String[] args) {
         
-        
+        ArrayList<String> array = new ArrayList<>();
+
+        array.add("John");
+        array.add("Nuggets");
+        array.add("Max");
+        for (String i : array) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+
+        String[] arrayStrings = {"John", "Chicken", "Burger"};
+
+        for (String string : arrayStrings) {
+            System.out.println(string);
+        }
     }
 }
