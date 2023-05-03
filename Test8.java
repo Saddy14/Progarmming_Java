247d238e5ff337f6f85542d8af4e539557fd40cd
Saddy14 <1221301874@student.mmu.edu.my>
2023-05-03 14:15:48 +0800

    Test8

--- Test8.java
+++ Test8.java
@@ -1,6 +1,45 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Test8 {
     public static void main(String[] args) {
         
+        String[] suit = { "c", "d", "h", "s" };
+        String[] rank = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "X", "J", "Q", "K" };
+
+        ArrayList<String> deck = new ArrayList<>();
+  
+        // for (int i = 0; i < 52; i++) {
+  
+        //    deck.add(rank[i%13] + suit[i/13]);
+        // }
+        for (int i = 0; i < suit.length; i++) {
+
+            for (int j = 0; j < rank.length; j++) {
+                deck.add(rank[j] + suit[i]);
+            }
+        }
+        System.out.println(deck.size());
+
+
+
+
+        // Check to see of deck is correctly working
+        System.out.println(deck);
+  
+        // Random Deck
+        Random random = new Random();
+  
+        for(int i = 0; i < 52; i++) {
+  
+           int randomIndex = random.nextInt(52);
+           deck.set(i, deck.get(randomIndex));
+        }
+        
+        // Check to see of deck is correctly working
+        System.out.println();
+        System.out.println(deck);
+        System.out.println(deck.size());
     }
 }
 
