bf36ea687b97e293a26a5a6c105c5bd5749b11fa
Saddy14 <1221301874@student.mmu.edu.my>
2023-06-08 14:47:38 +0800

    lab12

--- Labs/Lab12/lab12.java
+++ Labs/Lab12/lab12.java
@@ -1,11 +1,13 @@
 package Labs.Lab12;
 
 import java.util.LinkedList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class lab12 {
     public static void main(String[] args) {
         
+        // Instance of the class 
         MyLinkedList list = new MyLinkedList();
 
         list.start();
@@ -18,22 +20,22 @@ class MyLinkedList {
 
     void start() {
 
-
         while (true) {
 
-            System.out.println("MyLinkedList: " + myLinkedList);
+            System.out.println("\nMyLinkedList: " + myLinkedList);
             System.out.println("Size: " + myLinkedList.size());
 
-            print();
+            printCommands();
             int userInput = input();
-            operation(userInput);
-            
-           
 
+            if( userInput == 0) {
+                break;
+            }
+            operation(userInput);
         }
     }
 
-    void print() {
+    void printCommands() {
         System.out.println("1 - Add random integer as first");
         System.out.println("2 - Add random integer as last");
         System.out.println("3 - Add random integer at specified index");
@@ -49,30 +51,58 @@ class MyLinkedList {
     }
 
     int input() {
-        
-        Scanner scanner = new Scanner(System.in);
-
-        int input = scanner.nextInt();
-
-        // scanner.close();
-
-        return input;
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            return input;
     }
 
     void operation(int userInput) {
 
-        switch (userInput) {
-            case 1:
-                myLinkedList.addFirst(5);
-                break;
-            case 2:
-                myLinkedList.addLast(5);
-                break;
-            case 3:
-                myLinkedList.add(5, 5);
-            default:
-                break;
+        Random random = new Random();
+        int x = random.nextInt(99);
+        Scanner scanner = new Scanner(System.in);
+        int specifiedIndex = 0;
+
+        if(userInput == 3 || userInput == 6 || userInput == 9) {
+
+            System.out.print("Enter Index: ");
+            specifiedIndex = scanner.nextInt();
         }
 
+        switch (userInput) {
+            case 1:
+                myLinkedList.addFirst(x);
+                break;
+            case 2:
+                myLinkedList.addLast(x);
+                break;
+            case 3:
+                myLinkedList.add(specifiedIndex, x);
+                break;
+            case 4:
+                System.out.println("First Integer: "+myLinkedList.getFirst() + "\n");
+                break;
+            case 5:
+                System.out.println("Last Integer: "+myLinkedList.getLast() + "\n");
+                break;
+            case 6:
+                System.out.println(myLinkedList.get(specifiedIndex) + "\n");
+                break;
+            case 7:
+                myLinkedList.removeFirst();
+                break;
+            case 8: 
+                myLinkedList.removeLast();
+                break;
+            case 9:
+                myLinkedList.remove(specifiedIndex);
+                break;
+            case 10:
+                myLinkedList.clear();
+                break;
+            default:
+                System.out.println("Invalid Command");
+                break;
+        }
     }
 }
